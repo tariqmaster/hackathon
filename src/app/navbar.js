28d5af1bc9de7a75618a39e301e6a6015d32f9f4
Muhammad Tariq <tariqchemengr@gmail.com>
2023-05-31 00:14:30 +0500

    Add Cart
    Cart added..

--- src/app/navbar.js
+++ src/app/navbar.js
@@ -1,18 +1,24 @@
-
-
 "use client"
 
 import logo from "../../public/logo.jpg";
 import Image from "next/image";
 import Link from 'next/link'
 import SearchBar from "./search-bar";
+import {CgShoppingCart} from "react-icons/cg"
+import React, { useState } from 'react'
+// import {RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+// import { useStateContext } from '../context/StateContext';
 
 
-export default function Navbar(){
+
+const Navbar=({Searchproducts})=>{
+  // const {showCart, setShowCart, totalQty} = useStateContext();
+  // const [toggleMenu, setToggleMenu] = useState(false);
+
     return(
         <nav className="">
             <div className=""> 
-           <ul className="flex justify-between content-center">
+           <ul className="flex justify-between content-end">
             <Link href='/'>
        <li> <Image src={logo} width={140} height={25} alt='logo' /></li>
       </Link>
@@ -21,10 +27,36 @@ export default function Navbar(){
         <Link href='/male'><li>Male</li></Link>
         <Link href='/kids'><li>Kids</li></Link>
         <Link href='/products'><li>All Products</li></Link>
+
+
       <li><SearchBar/></li>
+      <Link href='/cart'><li>
+        <button className='cart' onClick={() => setShowCart(false)}>   
+          <CgShoppingCart size={22} />
+          <span className='cart-item-qty'>
+            
+            {/* {totalQty} */}
+            0
+            </span> 
+        </button></li>
+      </Link> 
+
+
       </ul>
+
+      
+      {/* <button className='cart' onClick={() => setShowCart(true)}> 
+        <CgShoppingCart size={22} />
+        <span className='cart-item-qty'>
+          {/* {totalQty} */}
+          {/* 0
+          {/* </span>
+      </button>  */}
+      
       
       </div>
         </nav>
     )
 }
+
+export default Navbar
