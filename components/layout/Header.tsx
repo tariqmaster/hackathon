f1223170a909df53149d1ce23b2f272e9d26e6de
Muhammad Tariq <tariqchemengr@gmail.com>
2023-07-07 00:09:24 +0500

    Revert "Hello"
    This reverts commit 05a83aa3611ce346c7bf11a20f7a775356dd0200.

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import logo from "../../public/Logo.webp";
 import Image from "next/image";
+import Link from 'next/link';
+import {ShoppingCart} from "lucide-react";
 
 const Header = () => {
   return (
-    <div className='flex justify-between items-center'>
+    <div className='flex justify-between items-center py-6 px-8'>
         <Image src={logo}  alt="logo" className='w-40'/>
         <ul className='flex gap-x-3'>
-            <li className='text-lg'>Female</li>
-            <li className='text-lg'>Male</li>
-            <li className='text-lg'>Kids</li>
-            <li className='text-lg'>All Products</li>
+            <li className='text-lg'><Link href={"/"}>Female</Link></li>
+            <li className='text-lg'><Link href={"/"}>Male</Link></li>
+            <li className='text-lg'><Link href={"/"}>Kid</Link></li>
+            <li className='text-lg'><Link href={"/"}>All Products</Link></li>
         </ul> 
-        <div className='h-10 w-10 rounded-full bg-gray-200'>
-
+        <div className='h-10 w-10 rounded-full bg-gray-200 flex justify-center items-center'>
+            <ShoppingCart />
         </div>
 
     
