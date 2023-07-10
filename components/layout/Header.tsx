15ca29d8d5e12a07c42b5caffe44c7dffe16fec8
Muhammad Tariq <tariqchemengr@gmail.com>
2023-07-10 17:59:54 +0500

    header working started
    start again after recovery.

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -2,19 +2,25 @@ import React from 'react';
 import logo from "../../public/Logo.webp";
 import Image from "next/image";
 import { ShoppingCart } from 'lucide-react';
+import Link from 'next/link';
+// import Input from 
 
 const Header = () => {
   return (
     <div className='flex justify-between items-center'>
         <Image src={logo}  alt="logo" className='w-40'/>
         <ul className='flex gap-x-3'>
-            <li className='text-lg'>Female</li>
-            <li className='text-lg'>Male</li>
-            <li className='text-lg'>Kids</li>
-            <li className='text-lg'>All Products</li>
+            <li className='text-lg'><Link href="/">Female</Link></li>
+            <li className='text-lg'><Link href="/">Male</Link></li>
+            <li className='text-lg'><Link href="/">Kids</Link></li>
+            <li className='text-lg'><Link href="/">All Products</Link></li>
         </ul> 
-        <div className='h-10 w-10 rounded-full bg-gray-200'>
+<div>
+  
+</div>
 
+        <div className='h-10 w-10 rounded-full bg-gray-200'>
+          {/* <Input></Input> */}
         </div>
 
     
